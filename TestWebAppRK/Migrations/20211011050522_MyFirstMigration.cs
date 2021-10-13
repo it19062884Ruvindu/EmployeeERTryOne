887ca60b4aee474d4b77df1556477764b75c51a7
it19062884Ruvindu <it19062884@my.sliit.lk>
2021-10-13 10:31:49 +0530

    Add project files.

---
+++ TestWebAppRK/Migrations/20211011050522_MyFirstMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestWebAppRK.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Customer_Name = table.Column<string>(type: "varchar(500)", nullable: false),
+                    Mobile = table.Column<string>(type: "varchar(500)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(MAX)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(500)", nullable: false),
+                    InsertedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    InsertedId = table.Column<long>(type: "bigint", nullable: false),
+                    ModifiedId = table.Column<long>(type: "bigint", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<string>(type: "varchar(50)", nullable: true),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profile");
+        }
+    }
+}
---
+++ TestWebAppRK/Model/Profile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebAppRK.Model
+{
+    [Table(name: "Profile")]
+    public class Profile
+    {
+        [Key]
+        public long ID { get; set; }
+        [Column(TypeName = "varchar(500)")]
+        [Required]
+        public string Customer_Name { get; set; }
+        [Column(TypeName = "varchar(500)")]
+        [Required]
+        public string Mobile { get; set; }
+        [Column(TypeName = "varchar(MAX)")]
+        [Required]
+        public string Email { get; set; }
+        [Column(TypeName = "varchar(500)")]
+        [Required]
+        public string Password { get; set; }
+        [Column(TypeName = "datetime")]
+        [Required]
+        public DateTime InsertedDate { get; set; }
+        [Column(TypeName = "datetime")]
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+        [Column(TypeName = "bigint")]
+        public long InsertedId { get; set; }
+        [Column(TypeName = "bigint")]
+        public long ModifiedId { get; set; }
+        [Column(TypeName = "bit")]
+        public bool Active { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string Status { get; set; }
+        [Column(TypeName = "bit")]
+        public bool IsAvailable { get; set; }
+
+    }
+}
